eb6ef26870ba5fc4c772a923c9d6201c1fbcf438
Mike Potts <mike@bigdatafed.com>
2018-07-10 13:20:24 -0700

    Added build of tfengine to tf build script.

--- bin/buildtf.osx.sh
+++ bin/buildtf.osx.sh
@@ -1,3 +1,7 @@
+cd $GITDIR/tfengine
+mvn package install
+if (( $? != 0 )); then echo 'mvn package tfengine failed' && popd &&  exit 127; fi
+
 cd $GITDIR/tcpclient
 mvn package install
 if (( $? != 0 )); then echo 'mvn package tcpclient failed' && popd &&  exit 127; fi
@@ -6,6 +10,7 @@ cd $GITDIR/tfdma
 ./build.osx.sh
 # if [ "$BUILDOK" != "TRUE" ]; then echo 'tfdma build.osx.sh failed'; exit 1; fi
 mvn package install
+
 cd $GITDIR/tf
 mvn package install
 if (( $? != 0 )); then echo 'mvn package tf failed' && popd &&  exit 127; fi
