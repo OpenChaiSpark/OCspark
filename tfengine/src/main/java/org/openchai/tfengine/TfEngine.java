927f0c97f5ab7c8f779936e4118330637fea2f3c
Mike Potts <mike@bigdatafed.com>
2018-07-09 21:56:37 -0700

    Made TfEngine importable via pom dependency and package.

---
+++ tfengine/src/main/java/org/openchai/tfengine/TfEngine.java
@@ -0,0 +1,8 @@
+package org.openchai.tfengine
+
+public class TfEngine {
+    public static String processImage(String[] args) {
+        System.out.println("Hello I'm a TfEngine!");
+        return "Output from processImage";
+    }
+}
