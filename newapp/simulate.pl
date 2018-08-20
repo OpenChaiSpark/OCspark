0f45f0f3bb8552c5a30d28b263d50c4106533e0d
Mike Potts <mike@bigdatafed.com>
2018-08-20 11:40:54 -0700

    Added parsing of result data, and reporting of latest + aggregated stats.

--- newapp/simulate.pl
+++ newapp/simulate.pl
@@ -17,9 +17,9 @@ use strict;
 
 my %config = (
               TEST_IMAGE => "/shared/test.jpg",
-##              TEST_IMAGE => "/Users/mike/tmp/cat.jpg",
-#              PERIOD => 1000,   # milliseconds
-              PERIOD => 5000,   # milliseconds
+#              TEST_IMAGE => "/Users/mike/tmp/cat.jpg",
+              PERIOD => 1000,   # milliseconds
+#              PERIOD => 5000,   # milliseconds
               DEBUG => 0
              );
 
@@ -34,6 +34,12 @@ my $period_ms = $config{PERIOD};
 my $test_image = $config{TEST_IMAGE};
 my $image_counter = 0;
 my $last_result = "";
+my $total_results = 0;
+my $total_proc_time = 0;
+my $total_total_time = 0;
+my $total_delay_time = 0;
+my $total_pending_results = 0;
+my $total_pending_images = 0;
 
 print STDERR "Watching $input_dir\n";
 
@@ -75,6 +81,13 @@ sub getLatestImage {
 }
 
 
+sub parseResultLine {
+  my $line = shift;
+
+  chomp $line;
+
+  substr $line, rindex($line, ': ') + length ': ';
+}
 
 
 ## Submit an image and then wait out the rest of the period.
@@ -99,6 +112,40 @@ sub submitImage {
   my $n_new_results = scalar(@new_results);
 
   $last_result = $results[$#results];
+  $total_results += $n_new_results;
+
+  ## Parse data from the new results.  Example:
+  ##
+  ##   /data/tmp/test3.jpg: 30.087158203125
+  ##   total time (us): 266
+  ##   delay time (us): 27
+  ##   pending results: 0
+  ##   pending images: 1
+
+  my $filename;
+  my $proc_time;
+  my $total_time;
+  my $delay_time;
+  my $pending_results;
+  my $pending_images;
+
+  foreach my $result (@new_results) {
+    open(FH, $result) || die "Error: $!\n";
+    my @lines = <FH>;
+    chomp $lines[0];
+    ($filename, $proc_time) = split(": ", $lines[0]);
+    $total_time = parseResultLine($lines[1]);
+    $delay_time = parseResultLine($lines[2]);
+    $pending_results = parseResultLine($lines[3]);
+    $pending_images = parseResultLine($lines[4]);
+#    print STDERR "$filename, $proc_time, $total_time, $delay_time, $pending_results, $pending_images\n";
+
+    $total_proc_time += $proc_time;
+    $total_total_time += $total_time;
+    $total_delay_time += $delay_time;
+    $total_pending_results += $pending_results;
+    $total_pending_images += $pending_images;
+}
 
   ## Sleep for the rest of the period.
 
@@ -110,5 +157,11 @@ sub submitImage {
 
   ## Output to logfile.
 
-  print STDERR "$image_counter $n_new_results $period_ms $start_time $finish_time $delta_time\n";
+  my $mean_proc_time = $total_results > 0 ? $total_proc_time / $total_results : "";
+  my $mean_total_time = $total_results > 0 ? $total_total_time / $total_results : "";
+  my $mean_delay_time = $total_results > 0 ? $total_delay_time / $total_results : "";
+  my $mean_pending_results = $total_results > 0 ? $total_pending_results / $total_results : "";
+  my $mean_pending_images = $total_results > 0 ? $total_pending_images / $total_results : "";
+
+  print STDERR "n=$image_counter p=$period_ms d=$delta_time r=$n_new_results tr=$total_results tp=$proc_time tt=$total_time td=$delay_time tpr=$pending_results tpi=$pending_images mp=$mean_proc_time mt=$mean_total_time md=$mean_delay_time mpr=$mean_pending_results mpi=$mean_pending_images\n";
 }
