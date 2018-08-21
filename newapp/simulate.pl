ce30b9c864bcf2b7f5a7ed01b69c7fee4339feac
Mike Potts <mike@bigdatafed.com>
2018-08-20 21:08:25 -0700

    Support 3 modes: ANALYSE=0 (just write the next file and then sleep); ANALYSE=1 (write the next file then read the results directory and count the results, then sleep); ANALYSE=2 (write the next file, read the results directory, then analyse all the new files and report the instantaneous and aggregated statisics).  First is constant time; second slows with increasing #results, third slows faster with increasing #results.

--- newapp/simulate.pl
+++ newapp/simulate.pl
@@ -18,9 +18,9 @@ use strict;
 my %config = (
               TEST_IMAGE => "/shared/test.jpg",
 #              TEST_IMAGE => "/Users/mike/tmp/cat.jpg",
-              PERIOD => 100,   # milliseconds
+              PERIOD => 10,   # milliseconds
 #              PERIOD => 5000,   # milliseconds
-              ANALYSE => 1,
+              ANALYSE => 0,
               DEBUG => 0
              );
 
@@ -53,8 +53,6 @@ while (1) {
 sub milliTime { gettimeofday * 1000 }
 sub milliSleep { usleep($_[0] * 1000) }
 
-
-
 ## Returns a list of available files, sorted by last modification date.
 
 sub getFiles {
@@ -80,7 +78,6 @@ sub getLatestImage {
   pop @images;
 }
 
-
 sub parseResultLine {
   my $line = shift;
 
@@ -88,7 +85,6 @@ sub parseResultLine {
 
   substr $line, rindex($line, ': ') + length ': ';
 }
-
 
 ## Submit an image and then wait out the rest of the period.
 
@@ -103,75 +99,83 @@ sub submitImage {
 
   copy($test_image, $target) or die "Copy failed: $!";
 
-  ## Get all results that have appeared since the last period.
-
-  my @results = getResults($output_dir);
-  my @new_results = @results[$total_results..$#results];
-  my $n_new_results = scalar(@new_results);
-
-  my $finish_time;
   my $delta_time;
 
   if ($config{ANALYSE}) {
-    ## Parse data from the new results.  Example:
-    ##
-    ##   /data/tmp/test3.jpg: 30.087158203125
-    ##   total time (us): 266
-    ##   delay time (us): 27
-    ##   pending results: 0
-    ##   pending images: 1
+    ## Get all results that have appeared since the last period.
 
-    my $filename;
-    my $proc_time;
-    my $total_time;
-    my $delay_time;
-    my $pending_results;
-    my $pending_images;
+    my @results = getResults($output_dir);
+    my @new_results = @results[$total_results..$#results];
+    my $n_new_results = scalar(@new_results);
 
-    foreach my $result (@new_results) {
-      open(FH, $result) || die "Error: $!\n";
-      my @lines = <FH>;
-      chomp $lines[0];
-      ($filename, $proc_time) = split(": ", $lines[0]);
-      $total_time = parseResultLine($lines[1]);
-      $delay_time = parseResultLine($lines[2]);
-      $pending_results = parseResultLine($lines[3]);
-      $pending_images = parseResultLine($lines[4]);
+    if ($config{ANALYSE} > 1) {
+      ## Parse data from the new results.  Example:
+      ##
+      ##   /data/tmp/test3.jpg: 30.087158203125
+      ##   total time (us): 266
+      ##   delay time (us): 27
+      ##   pending results: 0
+      ##   pending images: 1
 
-      ## Aggregate.
+      my $filename;
+      my $proc_time;
+      my $total_time;
+      my $delay_time;
+      my $pending_results;
+      my $pending_images;
 
-      $total_results ++;
+      foreach my $result (@new_results) {
+        open(FH, $result) || die "Error: $!\n";
+        my @lines = <FH>;
+        chomp $lines[0];
+        ($filename, $proc_time) = split(": ", $lines[0]);
+        $total_time = parseResultLine($lines[1]);
+        $delay_time = parseResultLine($lines[2]);
+        $pending_results = parseResultLine($lines[3]);
+        $pending_images = parseResultLine($lines[4]);
 
-      $total_proc_time += $proc_time;
-      $total_total_time += $total_time;
-      $total_delay_time += $delay_time;
-      $total_pending_results += $pending_results;
-      $total_pending_images += $pending_images;
+        ## Aggregate.
 
-      my $mean_proc_time = $total_proc_time / $total_results;
-      my $mean_total_time = $total_total_time / $total_results;
-      my $mean_delay_time = $total_delay_time / $total_results;
-      my $mean_pending_results = $total_pending_results / $total_results;
-      my $mean_pending_images = $total_pending_images / $total_results;
+        $total_results ++;
+
+        $total_proc_time += $proc_time;
+        $total_total_time += $total_time;
+        $total_delay_time += $delay_time;
+        $total_pending_results += $pending_results;
+        $total_pending_images += $pending_images;
+
+        my $mean_proc_time = $total_proc_time / $total_results;
+        my $mean_total_time = $total_total_time / $total_results;
+        my $mean_delay_time = $total_delay_time / $total_results;
+        my $mean_pending_results = $total_pending_results / $total_results;
+        my $mean_pending_images = $total_pending_images / $total_results;
+
+        ## Update the time tracking.
+
+        my $finish_time = milliTime;
+        $delta_time = $finish_time - $start_time;
+
+        ## Output to logfile.
+
+        print STDERR "n=$image_counter p=$period_ms d=$delta_time tr=$total_results nr=$n_new_results pt=$proc_time tt=$total_time dt=$delay_time pr=$pending_results pi=$pending_images mpt=$mean_proc_time mtt=$mean_total_time mdt=$mean_delay_time mpr=$mean_pending_results mpi=$mean_pending_images\n";
+      }
+    } else {
+      $total_results += $n_new_results;
 
       ## Update the time tracking.
 
-      $finish_time = milliTime;
+      my $finish_time = milliTime;
       $delta_time = $finish_time - $start_time;
 
-      ## Output to logfile.
-
-      print STDERR "n=$image_counter p=$period_ms d=$delta_time tr=$total_results nr=$n_new_results pt=$proc_time tt=$total_time dt=$delay_time pr=$pending_results pi=$pending_images mpt=$mean_proc_time mtt=$mean_total_time mdt=$mean_delay_time mpr=$mean_pending_results mpi=$mean_pending_images\n";
+      print STDERR "n=$image_counter p=$period_ms d=$delta_time tr=$total_results nr=$n_new_results\n";
     }
   } else {
-    $total_results += $n_new_results;
-
     ## Update the time tracking.
 
-    $finish_time = milliTime;
+    my $finish_time = milliTime;
     $delta_time = $finish_time - $start_time;
 
-    print STDERR "n=$image_counter p=$period_ms d=$delta_time tr=$total_results nr=$n_new_results\n";
+    print STDERR "n=$image_counter p=$period_ms d=$delta_time\n";
   }
 
   ## Sleep for the rest of the period.
